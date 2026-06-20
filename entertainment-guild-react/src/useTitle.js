1f62f6d530f6a86c73c42049ecda625cb18f8132
Shubert Chan <1969515540@qq.com>
2026-06-21 01:55:41 +0800

    Delete entertainment-guild-react directory

--- entertainment-guild-react/src/useTitle.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-// Sets the document title for a page, mirroring the original per-page titles.
-export function useTitle(title) {
-  useEffect(() => {
-    document.title = `${title} | Wukong`;
-  }, [title]);
-}
